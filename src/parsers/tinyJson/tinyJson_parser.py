889966b404f2d88a49895b57ddbe057861d300fd
Jonas Ott <ott_jonas@gmx.de>
2025-04-24 13:13:11 +0200

    Created correct file

---
+++ src/parsers/tinyJson/tinyJson_parser.py
@@ -0,0 +1,47 @@
+from parsers.common import *
+
+type Json = str | int | dict[str, Json]
+
+def ruleJson(toks: TokenStream) -> Json:
+    return alternatives("json", toks, [ruleObject, ruleString, ruleInt])
+
+def ruleObject(toks: TokenStream) -> dict[str, Json]:
+    toks.ensureNext("LBRACE")
+    d = ruleEntryList(toks)
+    toks.ensureNext("RBRACE")
+    return d
+
+def ruleEntryList(toks: TokenStream) -> dict[str, Json]:
+    if toks.lookahead() == "STRING":
+        return ruleEntryListNotEmpty(toks)
+    else:
+        return {}
+
+def ruleEntryListNotEmpty(toks: TokenStream) -> dict[str, Json]:
+    d : dict[str,Json] = {}
+    e = ruleEntry(toks)
+    d.update([e])
+    if toks.lookahead() == "COMMA":
+        toks.next()
+        d.update(ruleEntryListNotEmpty(toks))
+    return d
+
+def ruleEntry(toks: TokenStream) -> tuple[str, Json]:
+    s = ruleString(toks)
+    toks.ensureNext("COLON")
+    return (s, ruleJson(toks))
+
+def ruleString(toks: TokenStream) -> str:
+    return str(toks.ensureNext("STRING").value[1:-1])
+
+def ruleInt(toks: TokenStream) -> int:
+    return int(toks.ensureNext("INT").value)
+
+def parse(code: str) -> Json:
+    parser = mkLexer("./src/parsers/tinyJson/tinyJson_grammar.lark")
+    tokens = list(parser.lex(code))
+    log.info(f'Tokens: {tokens}')
+    toks = TokenStream(tokens)
+    res = ruleJson(toks)
+    toks.ensureEof(code)
+    return res
