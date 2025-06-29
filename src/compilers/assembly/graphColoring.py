f7da2a3f5fc3b3f33d39a2adbc716aeb3edbd00a
Jonas Ott <ott_jonas@gmx.de>
2025-06-29 13:51:24 +0200

    Coloring working

--- src/compilers/assembly/graphColoring.py
+++ src/compilers/assembly/graphColoring.py
@@ -7,7 +7,10 @@ def chooseColor(x: tac.ident, forbidden: dict[tac.ident, set[int]]) -> int:
     """
     Returns the lowest possible color for variable x that is not forbidden for x.
     """
-    raise ValueError('implement me')
+    color = 0
+    while color in forbidden[x]:
+      color += 1
+    return color
 
 def colorInterfGraph(g: InterfGraph, secondaryOrder: dict[tac.ident, int]={},
                      maxRegs: int=MAX_REGISTERS) -> RegisterMap:
@@ -22,13 +25,20 @@ def colorInterfGraph(g: InterfGraph, secondaryOrder: dict[tac.ident, int]={},
     """
     log.debug(f"Coloring interference graph with maxRegs={maxRegs}")
     colors: dict[tac.ident, int] = {}
-    forbidden: dict[tac.ident, set[int]] = {}
+    forbidden: dict[tac.ident, set[int]] = {v: set() for v in g.vertices}
     q = PrioQueue(secondaryOrder)
     
     for v in g.vertices:
         q.push(v)
-        
     
+    while not q.isEmpty():
+      v = q.pop()
+      
+      color = chooseColor(v, forbidden)
+      colors[v] = color
+      for v2 in g.succs(v):
+        forbidden[v2].add(color)
+        q.incPrio(v2)
     
     m = RegisterAllocMap(colors, maxRegs)
     return m
